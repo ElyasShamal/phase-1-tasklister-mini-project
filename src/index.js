691b1f70c48161579383053b38c61e3a10785949
Elyas <Elyas.shamal2@gmail.com>
2023-04-15 22:38:17 -0400

    add colors

--- src/index.js
+++ src/index.js
@@ -15,25 +15,27 @@ function handleFormSubmit(e){
   e.preventDefault()
    
   const task = e.target /* ['new-task-description'] */ [0].value
-  
-  displayTask(task)
+  const priorityLevel = parseInt(e.target[1].value);
+  displayTask(task,priorityLevel)
 
 }
 
-function displayTask(task){
+function displayTask(task,priorityLevel){
   const taskUl = document.getElementById('tasks');
   const taskLi = document.createElement('li');
 
   taskLi.textContent = task + ' '
+  taskLi.style.color = getPriorityColor(priorityLevel);
   taskUl.appendChild(taskLi);
 
   const deleteBtn = document.createElement('button');
   deleteBtn.textContent = "X";
+  taskLi.appendChild(deleteBtn)
+   
   deleteBtn.style.backgroundColor = "red"
   deleteBtn.style.color = "white"
   deleteBtn.style.fontSize = '10px'
-  taskLi.appendChild(deleteBtn)
-
+ 
   deleteBtn.addEventListener('click', deleteTask)
  
   
@@ -41,4 +43,17 @@ function displayTask(task){
 
 function deleteTask(e){
    e.target.parentNode.remove();
+}
+
+function getPriorityColor(priorityLevel){
+
+  if(priorityLevel === 1){
+    return 'red';
+  }
+  else if(priorityLevel === 2){
+    return 'black';
+  }
+  else{
+    return 'blue'
+  }
 }
