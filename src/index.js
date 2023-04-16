3ed032a08207adaa48ff9806c348a6a87b6fa15e
Elyas <Elyas.shamal2@gmail.com>
2023-04-15 21:43:33 -0400

    display task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    addingEventListeners()
 });
+
+
+
+function addingEventListeners() {
+   document.getElementById('create-task-form')
+   .addEventListener('submit', handleFormSubmit);
+    
+}
+
+function handleFormSubmit(e){
+  e.preventDefault()
+   
+  const task = e.target /* ['new-task-description'] */ [0].value
+  
+  displayTask(task)
+
+}
+
+function displayTask(task){
+  const taskUl = document.getElementById('tasks');
+  const taskLi = document.createElement('li');
+ 
+  taskLi.textContent = task
+  taskUl.appendChild(taskLi);
+}
